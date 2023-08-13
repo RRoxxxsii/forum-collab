0ba51e9de489610602091c95ca288600115e7410
Kamil <windycate03@gmail.com>
2023-08-13 13:38:50 +0300

    refactoring&update: code refactoring and wait for test added

--- frontend/shared/QuestionItemWrapper/QuestionItemWrapper.tsx
+++ frontend/shared/QuestionItemWrapper/QuestionItemWrapper.tsx
@@ -1,16 +1,25 @@
 import { Card } from '@mui/material'
+import Link from 'next/link'
 import React from 'react'
 
 export const QuestionItemWrapper = ({
 	children,
+	href,
 }: {
 	children: React.ReactNode
+	href: string
 }) => {
 	return (
 		<Card
-			sx={{ width: 'clamp(300px, 100%, 1200px)', mb: 2 }}
-			className='hover:bg-gray-200'>
-			{children}
+			sx={{
+				width: 'clamp(300px, 100%, 1200px)',
+				transition: 'border-color 0.3s ease-in-out',
+				border: '1px solid transparent',
+				'&:hover': { border: 1, transition: 0.3, borderColor: 'teal' },
+				cursor: 'pointer',
+				mb: 2,
+			}}>
+			<Link href={href}>{children}</Link>
 		</Card>
 	)
 }
--- frontend/widgets/QuestionList/QuestionList.tsx
+++ frontend/widgets/QuestionList/QuestionList.tsx
@@ -1,41 +1,34 @@
 import { QuestionItemContent } from '@/entities/QuestionItemContent'
 import { QuestionItemActions } from '@/features/QuestionItemActions/QuestionItemActions'
 import { QuestionItemWrapper } from '@/shared/QuestionItemWrapper'
-
-interface IUser {
-	avatar: string
-	username: string
-	email: string
-}
-
-export interface IChip {
-	key: number
-	label: string
-}
-
-export interface IQuestionItem {
-	id: string | number
-	user: IUser
-	title: string
-	description: string
-	chips: IChip[]
-}
+import { IQuestionItem } from '@/types/types'
 
 export const QuestionList = () => {
 	const questions: IQuestionItem[] = [
 		{
-			id: 0,
-			user: { avatar: '', email: 'example@example.com', username: 'example' },
+			id: '0',
+			user: {
+				id: '0',
+				avatar: '',
+				email: 'example@example.com',
+				username: 'example',
+			},
 			chips: [
 				{ key: 0, label: 'jquery' },
 				{ key: 1, label: 'react' },
 			],
 			description: 'test description',
 			title: 'test title',
+			comments: [{}],
 		},
 		{
-			id: 1,
-			user: { avatar: '', email: 'example@example.com', username: 'example' },
+			id: '1',
+			user: {
+				id: '1',
+				avatar: '',
+				email: 'example@example.com',
+				username: 'example',
+			},
 			chips: [
 				{ key: 0, label: 'laravel' },
 				{ key: 1, label: 'php' },
@@ -44,19 +37,12 @@ export const QuestionList = () => {
 			description: 'test description',
 			title: 'test title',
 		},
-		{
-			id: 2,
-			user: { avatar: '', email: 'example@example.com', username: 'example' },
-			chips: [{ key: 0, label: 'html' }],
-			description: 'test description',
-			title: 'test title',
-		},
 	]
 
 	return (
 		<>
 			{questions.map((question) => (
-				<QuestionItemWrapper key={question.id}>
+				<QuestionItemWrapper href={question.id} key={question.id}>
 					<QuestionItemContent questionData={question} />
 					<QuestionItemActions chips={question.chips} />
 				</QuestionItemWrapper>
