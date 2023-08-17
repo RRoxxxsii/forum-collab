c199aed22ebc4fdcce5c85724a999abca6f25fba
rroxxxsii <mishabur38@gmail.com>
2023-08-17 18:26:39 +0300

    alter user register serializer

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -27,6 +27,7 @@ class RegisterUserSerializer(serializers.ModelSerializer):
         password = validated_data.pop('password', None)
         password = self.validate_password(password)
         instance = self.Meta.model(**validated_data)
+        instance.set_password(password)
         instance.save()
         return instance
 
