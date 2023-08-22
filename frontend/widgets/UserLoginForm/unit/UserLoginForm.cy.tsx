8e97e08f4942b140a945e5c86fe59a4f91e31474
Kamil <windycate03@gmail.com>
2023-08-22 05:42:15 +0300

    unit test fix: router mock added

--- frontend/widgets/UserLoginForm/unit/UserLoginForm.cy.tsx
+++ frontend/widgets/UserLoginForm/unit/UserLoginForm.cy.tsx
@@ -1,13 +1,53 @@
-import { UserLoginForm } from './UserLoginForm'
+import { UserLoginForm } from '../UserLoginForm'
+
+import {
+	AppRouterContext,
+	AppRouterInstance,
+} from 'next/dist/shared/lib/app-router-context'
+
+const createRouter = (params: Partial<AppRouterInstance>) => ({
+	back: cy.spy().as('back'),
+	forward: cy.spy().as('forward'),
+	prefetch: cy.stub().as('prefetch').resolves(),
+	push: cy.spy().as('push'),
+	replace: cy.spy().as('replace'),
+	refresh: cy.spy().as('refresh'),
+	...params,
+})
+
+interface MockNextRouterProps extends Partial<AppRouterInstance> {
+	children: React.ReactNode
+}
+
+const MockNextRouter = ({ children, ...props }: MockNextRouterProps) => {
+	const router = createRouter(props as AppRouterInstance)
+
+	return (
+		<AppRouterContext.Provider value={router}>
+			{children}
+		</AppRouterContext.Provider>
+	)
+}
+
+export default MockNextRouter
 
 describe('<UserLoginForm />', () => {
 	it('renders', () => {
 		// see: https://on.cypress.io/mounting-react
-		cy.mount(<UserLoginForm />)
+
+		cy.mount(
+			<MockNextRouter>
+				<UserLoginForm />
+			</MockNextRouter>
+		)
 	})
 	it('should successfully submit the form with valid data', () => {
 		// Fill in the email and password fields
-		cy.mount(<UserLoginForm />)
+		cy.mount(
+			<MockNextRouter>
+				<UserLoginForm />
+			</MockNextRouter>
+		)
 
 		cy.get('[data-cy=email]').type('test123@gmail.com')
 		cy.get('[data-cy=password]').type('test123')
@@ -19,7 +59,11 @@ describe('<UserLoginForm />', () => {
 	})
 
 	it('should display errors for empty fields', () => {
-		cy.mount(<UserLoginForm />)
+		cy.mount(
+			<MockNextRouter>
+				<UserLoginForm />
+			</MockNextRouter>
+		)
 
 		// Submit the form without filling in any fields
 		cy.get('[data-cy=submit-button]').click()
@@ -30,7 +74,11 @@ describe('<UserLoginForm />', () => {
 	})
 
 	it('should display error for invalid email', () => {
-		cy.mount(<UserLoginForm />)
+		cy.mount(
+			<MockNextRouter>
+				<UserLoginForm />
+			</MockNextRouter>
+		)
 		// Fill in an invalid email format
 		cy.get('[data-cy=email]').type('invalid-email')
 
@@ -42,7 +90,11 @@ describe('<UserLoginForm />', () => {
 	})
 
 	it('should display error for incorrect password', () => {
-		cy.mount(<UserLoginForm />)
+		cy.mount(
+			<MockNextRouter>
+				<UserLoginForm />
+			</MockNextRouter>
+		)
 		// Fill in the email and an incorrect password
 		cy.get('[data-cy=email]').type('test@example.com')
 		cy.get('[data-cy=password]').type('123')
