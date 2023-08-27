26de06c4cd16a18e45d287c32653f2daa5eb50f9
rroxxxsii <mishabur38@gmail.com>
2023-08-27 16:22:38 +0300

    renamed field tag to tag_name

--- backend/forum/serializers.py
+++ backend/forum/serializers.py
@@ -29,7 +29,7 @@ class TagFieldSerializer(serializers.ModelSerializer):
     use_count = serializers.SerializerMethodField()
 
     class Meta:
-        fields = ('tag', 'use_count', 'is_relevant', 'is_user_tag')
+        fields = ('tag_name', 'use_count', 'is_relevant', 'is_user_tag')
         model = ThemeTag
 
     def get_use_count(self, obj: ThemeTag):
