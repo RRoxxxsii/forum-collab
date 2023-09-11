f408e3d730efd8f3457260a7b6a54c6014f02b36
rroxxxsii <mishabur38@gmail.com>
2023-09-11 10:31:28 +0300

    refactored createdata

--- backend/forum/management/commands/createdata.py
+++ backend/forum/management/commands/createdata.py
@@ -9,7 +9,116 @@ from forum.models import (AnswerComment, Question, QuestionAnswer,
                           QuestionAnswerImages, QuestionImages, ThemeTag)
 
 
-class Helper:
+class QuestionHelper:
+
+    def __init__(self, questions_amount, users, tags):
+        self.questions_amount = questions_amount
+        self.users = users
+        self.tags = tags
+        self.fake = Faker(['ru_RU'])
+
+    def create_questions(self):
+        for _ in range(self.questions_amount):
+            users_sorted = self.users.order_by('?')
+            tags_sorted = self.tags.order_by('?')[:random.randint(1, 5)]
+
+            question = Question.objects.create(
+                user=users_sorted.first(),
+                title=self.fake.text(max_nb_chars=200),
+                content=self.fake.paragraph(nb_sentences=7),
+                is_solved=self.fake.pybool()
+            )
+
+            for tag in tags_sorted:
+                question.tags.add(tag)
+            question.save()
+
+            self.__create_question_images(question)
+            self.__create_question_rating(question, users_sorted=users_sorted)
+
+        questions = Question.objects.all()
+
+        return questions
+
+    def __create_question_images(self, question):
+        for _ in range(random.randint(0, 3)):
+            flag = random.randint(0, 1)
+            question_image = QuestionImages.objects.create(
+                parent=question,
+                image=self.fake.file_name(category='image', extension='jpeg')
+            )
+            if flag:
+                question_image.alt = self.fake.text(max_nb_chars=200)
+            question_image.save()
+
+    def __create_question_rating(self, question, users_sorted):
+        users_liked = list(users_sorted[:random.randint(0, self.users.count())])
+        users_disliked = []
+        for user in users_sorted[:random.randint(0, self.users.count())]:
+            if user not in users_liked:
+                users_disliked.append(user)
+
+        question.rating.users_liked.add(*users_liked)
+        question.rating.users_disliked.add(*users_disliked)
+        question.rating.like_amount = len(users_liked)
+        question.rating.dislike_amount = len(users_disliked)
+        question.rating.save()
+        question.save()
+
+
+class AnswerHelper:
+
+    def __init__(self, users, questions):
+        self.users = users
+        self.questions = questions
+        self.fake = Faker(['ru_RU'])
+
+    def create_answers(self):
+        for question in self.questions:
+            flag = random.randint(0, 1)
+            users_sorted = self.users.order_by('?')
+
+            if flag:
+                answer = QuestionAnswer.objects.create(
+                    question=question, answer=self.fake.paragraph(nb_sentences=7)
+                )
+                user_identified = random.randint(0, 1)
+
+                if user_identified:
+                    answer.user = users_sorted.first()
+                    answer.save()
+
+                self.__create_answer_images(answer=answer)
+                self.__create_answer_rating(answer=answer, users_sorted=users_sorted)
+
+        answers = QuestionAnswer.objects.all()
+
+        return answers
+
+    def __create_answer_images(self, answer):
+        generate_image = random.randint(0, 1)
+        if generate_image:
+            QuestionAnswerImages.objects.create(
+                parent=answer,
+                image=self.fake.file_name(category='image', extension='jpeg')
+            )
+
+    def __create_answer_rating(self, answer, users_sorted):
+        users_liked = list(users_sorted[:random.randint(0, self.users.count())])
+        users_disliked = []
+        for user in users_sorted[:random.randint(0, self.users.count())]:
+            if user not in users_liked:
+                users_disliked.append(user)
+
+        answer.rating.users_liked.add(*users_liked)
+        answer.rating.users_disliked.add(*users_disliked)
+        answer.rating.like_amount = len(users_liked)
+        answer.rating.dislike_amount = len(users_disliked)
+        answer.rating.save()
+        answer.save()
+
+
+class Helper(AnswerHelper, QuestionHelper):
 
     def create_tags(self, tags_amount):
         fake = Faker(['ru_RU'])
@@ -51,89 +160,6 @@ class Helper:
 
         return users
 
-    def create_questions(self, questions_amount, users, tags):
-        fake = Faker(['ru_RU'])
-
-        for _ in range(questions_amount):
-            users_ = users.order_by('?')
-            tags_ = tags.order_by('?')[:random.randint(1, 5)]
-
-            question = Question.objects.create(user=users_.first(), title=fake.text(max_nb_chars=200),
-                                               content=fake.paragraph(nb_sentences=7),
-                                               is_solved=fake.pybool())
-
-            for tag in tags_:
-                question.tags.add(tag)
-            question.save()
-
-            for _ in range(random.randint(0, 3)):
-                flag = random.randint(0, 1)
-                question_image = QuestionImages.objects.create(
-                    parent=question,
-                    image=fake.file_name(category='image', extension='jpeg')
-                )
-                if flag:
-                    question_image.alt = fake.text(max_nb_chars=200)
-                question_image.save()
-
-            users_liked = list(users_[:random.randint(0, users.count())])
-            users_disliked = []
-            for user in users_[:random.randint(0, users.count())]:
-                if user not in users_liked:
-                    users_disliked.append(user)
-
-            question.rating.users_liked.add(*users_liked)
-            question.rating.users_disliked.add(*users_disliked)
-            question.rating.like_amount = len(users_liked)
-            question.rating.dislike_amount = len(users_disliked)
-            question.rating.save()
-            question.save()
-
-        questions = Question.objects.all()
-        self.stdout.write(self.style.SUCCESS(f'Число вопросов: {questions.count()}.'))
-
-        return questions
-
-    def create_answers(self, questions, users):
-        fake = Faker(['ru_RU'])
-
-        for question in questions:
-            flag = random.randint(0, 1)
-            users_ = users.order_by('?')
-
-            if flag:
-                answer = QuestionAnswer.objects.create(question=question, answer=fake.paragraph(nb_sentences=7))
-                user_identified = random.randint(0, 1)
-
-                if user_identified:
-                    answer.user = users_.first()
-                    answer.save()
-
-                generate_image = random.randint(0, 1)
-                if generate_image:
-                    QuestionAnswerImages.objects.create(
-                        parent=answer,
-                        image=fake.file_name(category='image', extension='jpeg')
-                    )
-
-                users_liked = list(users_[:random.randint(0, users.count())])
-                users_disliked = []
-                for user in users_[:random.randint(0, users.count())]:
-                    if user not in users_liked:
-                        users_disliked.append(user)
-
-                answer.rating.users_liked.add(*users_liked)
-                answer.rating.users_disliked.add(*users_disliked)
-                answer.rating.like_amount = len(users_liked)
-                answer.rating.dislike_amount = len(users_disliked)
-                answer.rating.save()
-                answer.save()
-
-        answers = QuestionAnswer.objects.all()
-        self.stdout.write(self.style.SUCCESS(f'Число ответов: {answers.count()}.'))
-
-        return answers
-
     def create_comments(self, answers, users):
         fake = Faker(['ru_RU'])
 
@@ -165,6 +191,13 @@ class Command(BaseCommand, Helper):
     def handle(self, *args, **options):
         tags = self.create_tags(tags_amount=15)
         users = self.create_users(users_amount=20)
-        questions = self.create_questions(questions_amount=50, users=users, tags=tags)
-        answers = self.create_answers(questions=questions, users=users)
+
+        q = QuestionHelper(questions_amount=50, users=users, tags=tags)
+        questions = q.create_questions()
+        self.stdout.write(self.style.SUCCESS(f'Число вопросов: {questions.count()}.'))
+
+        a = AnswerHelper(users=users, questions=questions)
+        answers = a.create_answers()
+        self.stdout.write(self.style.SUCCESS(f'Число ответов: {answers.count()}.'))
+
         self.create_comments(answers=answers, users=users)
