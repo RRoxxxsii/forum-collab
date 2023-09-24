d02e1058490f1ad7dade11f0f3e14f0e7c279003
rroxxxsii <mishabur38@gmail.com>
2023-09-24 21:18:11 +0300

    notification endpoint

--- backend/notifications/serializers.py
+++ backend/notifications/serializers.py
@@ -8,3 +8,4 @@ class NotificationsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Notification
         fields = '__all__'
+        extra_kwargs = {'target_content_type': {'required': False}}
