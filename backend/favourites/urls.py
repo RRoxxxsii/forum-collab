ff9bba0d7aa50e9db04fe0e5bd8111d3eb06073f
RRoxxxsii <mishabur38@gmail.com>
2023-10-07 13:33:20 +0300

    extended favourites functionality

--- backend/favourites/urls.py
+++ backend/favourites/urls.py
@@ -4,6 +4,7 @@ from favourites import views
 
 
 urlpatterns = [
-    path('favourites-add/', views.AddToFavouritesAPIView.as_view(), name='add-to-favourites')
+    path('favourites-add/', views.AddToFavouritesAPIView.as_view(), name='add-to-favourites'),
+    path('favourites/', views.FavouritesListAPIView.as_view(), name='favourites')
 ]
 
