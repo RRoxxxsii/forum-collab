2ab44caf9a87d50b835d8e9e9c411adc193e6997
RRoxxxsii <mishabur38@gmail.com>
2023-10-13 10:26:03 +0300

    added throttling

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -172,7 +172,15 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-    )
+    ),
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '20/minute',
+        'user': '50/minute'
+    }
 }
 
 
